67d2453c815f15d008b909a0a4e5d7ea6a68499c
Mathew Markose <mathewmarkose.a@gmail.com>
2017-04-18 23:18:36 +0530

    RichtextBox Controll Added and Student Add page Added

---
+++ S360Controlls/BasicControls/S360RichTextBox.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace S360Controlls.BasicControls
+{
+    public class S360RichTextBox : RichTextBox
+    {
+        /// <summary>
+        /// Overridden method to set background color as Aqua
+        /// when mouse focus on control
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            this.Background = Brushes.Aqua;
+        }
+        /// <summary>
+        /// Overridden method to set background color as Aqua
+        /// when keyboard focus on control
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnGotKeyboardFocus(KeyboardFocusChangedEventArgs e)
+        {
+            base.OnGotKeyboardFocus(e);
+            this.Background = Brushes.Aqua;
+        }
+        /// <summary>
+        /// Overridden method to set background color as White
+        /// when mouse focus lost from control
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            this.Background = Brushes.White;
+        }
+        /// <summary>
+        /// Overridden method to set background color as White
+        /// when keyboard focus lost from control
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnLostKeyboardFocus(KeyboardFocusChangedEventArgs e)
+        {
+            base.OnLostKeyboardFocus(e);
+            this.Background = Brushes.White;
+        }
+    }
+}
